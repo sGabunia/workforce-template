d224ff18b1b6947c7904a54125a5727d7b88a438
Gabunia Sergo <gabuniasergo@Gabunias-MacBook-Air.local>
2026-01-19 14:10:58 +0400

    Update url

--- react-router.config.ts
+++ react-router.config.ts
@@ -5,5 +5,5 @@ export default {
   // Server-side render by default, to enable SPA mode set this to `false`
   ssr: false,
   appDirectory: 'src/app',
-  basename: '/workforce-template/',
+  basename: '/workforce-template',
 } satisfies Config;
